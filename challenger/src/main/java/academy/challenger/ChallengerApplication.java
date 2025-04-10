10e811c747b2a8895b586f58e28d69276374cd84
seongm1n <0411tjdals34@gmail.com>
2025-04-10 21:00:45 +0900

    initial commit

---
+++ challenger/src/main/java/academy/challenger/ChallengerApplication.java
@@ -0,0 +1,13 @@
+package academy.challenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengerApplication.class, args);
+	}
+
+}
