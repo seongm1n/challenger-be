2269b489200919417fabb3a94c4ac38aacf22485
seongm1n <0411tjdals34@gmail.com>
2025-04-11 21:23:56 +0900

    feat: h2 의존성 추가

--- challenger/src/main/java/academy/challenger/user/User.java
+++ challenger/src/main/java/academy/challenger/user/User.java
@@ -1,13 +1,21 @@
 package academy.challenger.user;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 
 @Entity
 @Getter
 public class User {
-    private final Long id;
-    private final String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public User() {
+    }
 
     public User(Long id, String name) {
         this.id = id;
