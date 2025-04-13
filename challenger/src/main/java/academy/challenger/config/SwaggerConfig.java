b3b2c40f36b23bd1dba0f9c7504e422835e54433
seongm1n <0411tjdals34@gmail.com>
2025-04-13 11:25:35 +0900

    feat: Swagger API 설정

---
+++ challenger/src/main/java/academy/challenger/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package academy.challenger.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Challenger API")
+                .description("챌린저 서비스의 API 명세입니다.")
+                .version("1.0.0");
+    }
+}
